3492028f1e3cd248f2d5025ad678edff238da2d2
Michal Bali <michal.bali@broadcom.com>
2019-11-26 13:15:00 +0100

    Open source prepare (#68)
    * jni removal
    
    * private test removal
    
    * c++ files license headers
    
    * grammar and cmake files license headers
    
    * remove unnecessary dependencies
    
    * gtest changed to released version
    
    * unnecessary script removal

--- cmake/external_gtest.cmake
+++ cmake/external_gtest.cmake
@@ -1,3 +1,15 @@
+# Copyright (c) 2019 Broadcom.
+# The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Broadcom, Inc. - initial API and implementation
+
 cmake_minimum_required (VERSION 3.10)
 
 
@@ -8,7 +20,7 @@ set(GTEST_EXTERNAL_ROOT ${CMAKE_BINARY_DIR}/externals/googletest)
 include(ExternalProject)
 ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
-  GIT_TAG           880896c6f4814f4c7798355a652dc6167be2b75f
+  GIT_TAG           release-1.10.0
   SOURCE_DIR        ${CMAKE_BINARY_DIR}/googletest-src
   BINARY_DIR        ${CMAKE_BINARY_DIR}/googletest-build
   CONFIGURE_COMMAND ""
--- parser_library/jni/export/parser_library_export.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef PARSER_LIBRARY_EXPORT_H
-#define PARSER_LIBRARY_EXPORT_H
-
-#  ifndef PARSER_LIBRARY_EXPORT
-#     define PARSER_LIBRARY_EXPORT
-#  endif
-
-#endif /* PARSER_LIBRARY_EXPORT_H */
