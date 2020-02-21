46cf29d7e75e655cbe20f530182f161a1f46ee2a
Michal Bali <michalbali32@gmail.com>
2020-02-21 15:09:21 +0100

    license check

---
+++ cmake/check_license.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Copyright (c) 2019 Broadcom.
+# The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Broadcom, Inc. - initial API and implementation
+
+
+SOURCES=$(find -name *.cpp -or -name *.h -or -name *.js -or -name *.ts -or -name *.yml -or -name CMakeLists.txt)
+
+RET=0
+for f in $SOURCES
+do
+  head $f | grep "Eclipse Public License 2.0" > /dev/null
+  if [ $? -ne 0 ]; then
+    RET=1
+    echo FAILED: $f
+  fi
+done
+
+return $RET
