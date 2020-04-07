21550ff2e44b06fb0f627578fd62b35f168acdc6
Michal Bali <michalbali32@gmail.com>
2020-04-07 12:19:53 +0200

    .cpp tests

---
+++ parser_library/test/diagnosable_ctx_test.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+#include "gtest/gtest.h"
+
+#include "common_testing.h"
+
+//test for
+//correctly outputed structure of nested diagnostic
+
+TEST(diagnosable_ctx, one_file_diag)
+{
+	std::string input = 
+R"( MACRO
+ M2
+ lr 1,
+ MEND
+
+ MACRO
+ M1
+ lr 1,1
+ M2
+ anop
+ mend
+
+ lr 1,1
+ M1
+)";
+	analyzer a(input);
+	a.analyze();
+
+	a.collect_diags();
+	ASSERT_EQ(a.diags().size(), (size_t)1);
+	
+	EXPECT_EQ(a.diags()[0].diag_range.start.line, (position_t) 2);
+	EXPECT_EQ(a.diags()[0].related.size(), (size_t) 2);
+	EXPECT_EQ(a.diags()[0].related[0].location.rang.start.line, (position_t) 8);
+	EXPECT_EQ(a.diags()[0].related[1].location.rang.start.line, (position_t) 13);
+}
---
+++ parser_library/test/fuzzer_test.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2019 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+#include "gtest/gtest.h"
+
+#include "common_testing.h"
+
+TEST(fuzzer_test, entry_1)
+{
+	std::string input(R"(:'0:', A $sMNOP A
+T (.~A
+n N ICT
+M(A la s
+ NL
+gA 
+~a s
+A la s
+&Q,EU,,,T  &$=>"+<+++-
+
+
+
+ COMM&u
+&z[&%
+
+
+
+%'l-**$D'  M C'&CAd
+;
+N ORG (.(. ))C )");
+
+	analyzer a(input);
+	a.analyze();
+}
+
+TEST(fuzzer_test, entry_2)
+{
+    std::string input(" ACTR LL'BTM 'M('SM''S(~''#S)((");
+
+    analyzer a(input);
+    a.analyze();
+}
+
+TEST(fuzzer_test, entry_3)
+{
+    std::string input(" (_=&LOCTRCEv(&ISEQ(--S'#)HUBP #' #&3#");
+
+    analyzer a(input);
+    a.analyze();
+}
+
+TEST(fuzzer_test, entry_4)
+{
+    std::string input(R"( @(PUSH@(&@(@+( ED'))D))))))))))9@(&@(@+( ED'BRXH))p(r
+R
+)");
+
+    analyzer a(input);
+    a.analyze();
+}
