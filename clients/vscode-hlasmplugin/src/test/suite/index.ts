0b3d4051622b4fac0cca0a4b528dcbc26e9ae9e3
Marcel Hruska <marcel1hruska@gmail.com>
2020-04-14 13:29:36 +0200

    fixed testing

--- clients/vscode-hlasmplugin/src/test/suite/index.ts
+++ clients/vscode-hlasmplugin/src/test/suite/index.ts
@@ -6,6 +6,7 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+import * as vscode from 'vscode';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -14,23 +15,25 @@ export function run(): Promise<void> {
 
 	return new Promise((resolve, reject) => {
 		glob('**/**.test.js', { cwd: testsPath }, (_, files) => {
-			// Add files to the test suite
-			files.forEach(file => 
-				mocha.addFile(path.resolve(testsPath, file)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						reject(new Error(`${failures} tests failed.`));
-					} else {
-						resolve();
-					}
-				});
-			} catch (error) {
-				console.error(error);
-				reject(error);
-			}
+				// Add files to the test suite
+				files.forEach(file => 
+					mocha.addFile(path.resolve(testsPath, file)));
+	
+				try {
+					vscode.workspace.getConfiguration('hlasmplugin').update('continuationHandling',true).then(() => {
+						// Run the mocha test
+						mocha.run(failures => {
+							if (failures > 0) {
+								reject(new Error(`${failures} tests failed.`));
+							} else {
+								resolve();
+							}
+						});
+					});
+				} catch (error) {
+					console.error(error);
+					reject(error);
+				}
 		});
 	});
 }
