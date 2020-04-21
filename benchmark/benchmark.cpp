38da7cd03c893d0aed3a1a9405b90eaee50f420f
Michal Bali <michalbali32@gmail.com>
2020-04-21 17:37:05 +0200

    benchmark now does correctly continues when there is an error & refactor

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -87,6 +87,115 @@ public:
     hlasm_plugin::parser_library::performance_metrics metrics_;
 };
 
+struct all_file_stats
+{
+    double average_line_ms = 0;
+    double average_stmt_ms = 0;
+    size_t all_files = 0;
+    long long whole_time = 0;
+    size_t program_count = 0;
+    size_t parsing_crashes = 0;
+    size_t failed_file_opens = 0;
+};
+
+void one_file(const std::string & source_file, json& result, const std::string ws_folder, all_file_stats& s)
+{
+    auto source_path = ws_folder + "/" + source_file;
+    std::ifstream in(source_path);
+    if (in.fail())
+    {
+        ++s.failed_file_opens;
+        std::clog << "File read error: " << source_path << std::endl;
+        result.push_back(json({ { "File", source_file }, { "Success", false } }));
+        return;
+    }
+    s.program_count++;
+    // program's contents
+    auto content = std::string((std::istreambuf_iterator<char>(in)), (std::istreambuf_iterator<char>()));
+    content = hlasm_plugin::parser_library::workspaces::file_impl::replace_non_utf8_chars(content);
+
+    // new workspace manager
+    hlasm_plugin::parser_library::workspace_manager ws;
+    diagnostic_counter consumer;
+    ws.register_diagnostics_consumer(&consumer);
+    metrics_collector collector;
+    ws.register_performance_metrics_consumer(&collector);
+    // input folder as new workspace
+    ws.add_workspace(ws_folder.c_str(), ws_folder.c_str());
+
+    // start counting
+    auto c_start = std::clock();
+    auto start = std::chrono::high_resolution_clock::now();
+    std::clog << "Parsing file: " << source_file << std::endl;
+    // open file/parse
+    try
+    {
+        ws.did_open_file(source_path.c_str(), 1, content.c_str(), content.length());
+    }
+    catch (std::exception& e)
+    {
+        ++s.parsing_crashes;
+        std::clog << "Error: " << e.what() << std::endl;
+        result.push_back(json({ { "File", source_file }, { "Success", false } }));
+        return;
+    }
+    catch (...)
+    {
+        ++s.parsing_crashes;
+        std::clog << "Parse failed\n\n" << std::endl;
+        result.push_back(json({ { "File", source_file }, { "Success", false } }));
+        return;
+    }
+
+    auto c_end = std::clock();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    auto exec_statements = collector.metrics_.open_code_statements + collector.metrics_.copy_statements
+        + collector.metrics_.macro_statements + collector.metrics_.lookahead_statements
+        + collector.metrics_.reparsed_statements;
+    s.average_stmt_ms += (exec_statements / (double)time);
+    s.average_line_ms += collector.metrics_.lines / (double)time;
+    s.all_files += collector.metrics_.files;
+    s.whole_time += time;
+
+    std::clog << "Time: " << time << " ms" << '\n'
+              << "Errors: " << consumer.error_count << '\n'
+              << "Open Code Statements: " << collector.metrics_.open_code_statements << '\n'
+              << "Copy Statements: " << collector.metrics_.copy_statements << '\n'
+              << "Macro Statements: " << collector.metrics_.macro_statements << '\n'
+              << "Copy Def Statements: " << collector.metrics_.copy_def_statements << '\n'
+              << "Macro Def Statements: " << collector.metrics_.macro_def_statements << '\n'
+              << "Lookahead Statements: " << collector.metrics_.lookahead_statements << '\n'
+              << "Reparsed Statements: " << collector.metrics_.reparsed_statements << '\n'
+              << "Continued Statements: " << collector.metrics_.continued_statements << '\n'
+              << "Non-continued Statements: " << collector.metrics_.non_continued_statements << '\n'
+              << "Lines: " << collector.metrics_.lines << '\n'
+              << "Executed Statement/ms: " << exec_statements / (double)time << '\n'
+              << "Line/ms: " << collector.metrics_.lines / (double)time << '\n'
+              << "Files: " << collector.metrics_.files << "\n\n"
+              << std::endl;
+
+    result.push_back(json({ { "File", source_file },
+        { "Success", true },
+        { "Errors", consumer.error_count },
+        { "Warnings", consumer.warning_count },
+        { "Wall Time (ms)", time },
+        { "CPU Time (ms/n)", 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC },
+        { "Open Code Statements: ", collector.metrics_.open_code_statements },
+        { "Copy Statements: ", collector.metrics_.copy_statements },
+        { "Macro Statements: ", collector.metrics_.macro_statements },
+        { "Copy Def Statements: ", collector.metrics_.copy_def_statements },
+        { "Macro Def Statements: ", collector.metrics_.macro_def_statements },
+        { "Lookahead Statements: ", collector.metrics_.lookahead_statements },
+        { "Reparsed Statements: ", collector.metrics_.reparsed_statements },
+        { "Continued Statements: ", collector.metrics_.continued_statements },
+        { "Non-continued Statements: ", collector.metrics_.non_continued_statements },
+        { "Lines", collector.metrics_.lines },
+        { "ExecStatement/ms", exec_statements / (double)time },
+        { "Line/ms", collector.metrics_.lines / (double)time },
+        { "Files", collector.metrics_.files } }));
+}
+
 int main(int argc, char** argv)
 {
     std::string ws_folder = std::filesystem::current_path().string();
@@ -148,20 +257,31 @@ int main(int argc, char** argv)
     // configuration contents
     std::string conf((std::istreambuf_iterator<char>(in)), (std::istreambuf_iterator<char>()));
 
+    json programs;
     try
     {
         // parsed to json
         json conf_json = json::parse(conf);
         // list of programs
-        std::vector<json> programs = conf_json["pgms"];
-        // results
-        json result = json::array();
-        // final results
-        double average_line_ms = 0;
-        double average_stmt_ms = 0;
-        size_t all_files = 0;
-        long long whole_time = 0;
-        size_t program_count = 0;
+        programs = conf_json["pgms"];
+    }
+    catch (...)
+    {
+        std::clog << "Malformed json" << std::endl;
+        return 1;
+    }
+    // results
+    json result = json::array();
+    all_file_stats s;
+    if (single_file != "")
+    {
+        if (end_range == 0)
+            end_range = LLONG_MAX;
+        for (size_t i = 0; i < end_range; ++i)
+            one_file(single_file, result, ws_folder, s);
+    }
+    else
+    {
         size_t current_iter = 0;
         for (auto program : programs)
         {
@@ -170,122 +290,44 @@ int main(int argc, char** argv)
                 current_iter++;
                 continue;
             }
+            std::string source_file;
             // program file
-            auto source_file = program["program"].get<std::string>();
-            if (single_file != "" && source_file != single_file)
-                continue;
-            while (true)
+            if (program.find("program") != program.end())
+                source_file = program["program"].get<std::string>();
+            else
             {
-                auto source_path = ws_folder + "/" + source_file;
-                in = std::ifstream(source_path);
-                if (in.fail())
-                    continue;
-                program_count++;
-                // program's contents
-                auto content = std::string((std::istreambuf_iterator<char>(in)), (std::istreambuf_iterator<char>()));
-                content = hlasm_plugin::parser_library::workspaces::file_impl::replace_non_utf8_chars(content);
-
-                // new workspace manager
-                hlasm_plugin::parser_library::workspace_manager ws;
-                diagnostic_counter consumer;
-                ws.register_diagnostics_consumer(&consumer);
-                metrics_collector collector;
-                ws.register_performance_metrics_consumer(&collector);
-                // input folder as new workspace
-                ws.add_workspace(ws_folder.c_str(), ws_folder.c_str());
-
-                // start counting
-                auto c_start = std::clock();
-                auto start = std::chrono::high_resolution_clock::now();
-                std::clog << "Parsing file: " << source_file << std::endl;
-                // open file/parse
-                try
-                {
-                    ws.did_open_file(source_path.c_str(), 1, content.c_str(), content.length());
-                }
-                catch (...)
-                {
-                    std::clog << "Parse failed\n\n" << std::endl;
-                    result.push_back(json({ { "File", source_file }, { "Success", false } }));
-                    continue;
-                }
-                auto c_end = std::clock();
-                auto end = std::chrono::high_resolution_clock::now();
-                auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-                auto exec_statements = collector.metrics_.open_code_statements + collector.metrics_.copy_statements
-                    + collector.metrics_.macro_statements + collector.metrics_.lookahead_statements
-                    + collector.metrics_.reparsed_statements;
-                average_stmt_ms += (exec_statements / (double)time);
-                average_line_ms += collector.metrics_.lines / (double)time;
-                all_files += collector.metrics_.files;
-                whole_time += time;
-
-                std::clog << "Time: " << time << " ms" << '\n'
-                          << "Errors: " << consumer.error_count << '\n'
-                          << "Open Code Statements: " << collector.metrics_.open_code_statements << '\n'
-                          << "Copy Statements: " << collector.metrics_.copy_statements << '\n'
-                          << "Macro Statements: " << collector.metrics_.macro_statements << '\n'
-                          << "Copy Def Statements: " << collector.metrics_.copy_def_statements << '\n'
-                          << "Macro Def Statements: " << collector.metrics_.macro_def_statements << '\n'
-                          << "Lookahead Statements: " << collector.metrics_.lookahead_statements << '\n'
-                          << "Reparsed Statements: " << collector.metrics_.reparsed_statements << '\n'
-                          << "Continued Statements: " << collector.metrics_.continued_statements << '\n'
-                          << "Non-continued Statements: " << collector.metrics_.non_continued_statements << '\n'
-                          << "Lines: " << collector.metrics_.lines << '\n'
-                          << "Executed Statement/ms: " << exec_statements / (double)time << '\n'
-                          << "Line/ms: " << collector.metrics_.lines / (double)time << '\n'
-                          << "Files: " << collector.metrics_.files << "\n\n"
-                          << std::endl;
-
-                result.push_back(json({ { "File", source_file },
-                    { "Success", true },
-                    { "Errors", consumer.error_count },
-                    { "Warnings", consumer.warning_count },
-                    { "Wall Time (ms)", time },
-                    { "CPU Time (ms/n)", 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC },
-                    { "Open Code Statements: ", collector.metrics_.open_code_statements },
-                    { "Copy Statements: ", collector.metrics_.copy_statements },
-                    { "Macro Statements: ", collector.metrics_.macro_statements },
-                    { "Copy Def Statements: ", collector.metrics_.copy_def_statements },
-                    { "Macro Def Statements: ", collector.metrics_.macro_def_statements },
-                    { "Lookahead Statements: ", collector.metrics_.lookahead_statements },
-                    { "Reparsed Statements: ", collector.metrics_.reparsed_statements },
-                    { "Continued Statements: ", collector.metrics_.continued_statements },
-                    { "Non-continued Statements: ", collector.metrics_.non_continued_statements },
-                    { "Lines", collector.metrics_.lines },
-                    { "ExecStatement/ms", exec_statements / (double)time },
-                    { "Line/ms", collector.metrics_.lines / (double)time },
-                    { "Files", collector.metrics_.files } }));
-                if (single_file == "")
-                    break;
+                ++s.failed_file_opens;
+                std::clog << "Malformed json" << std::endl;
+                continue;
             }
+            one_file(source_file, result, ws_folder, s);
+
             if (current_iter >= end_range && end_range > 0)
                 break;
             current_iter++;
         }
-
-        std::clog << "Programs: " << program_count << '\n'
-                  << "Benchmarked files: " << all_files << '\n'
-                  << "Benchmark time: " << whole_time << " ms" << '\n'
-                  << "Average statement/ms: " << average_stmt_ms / (double)programs.size() << '\n'
-                  << "Average line/ms: " << average_line_ms / (double)programs.size() << "\n\n"
-                  << std::endl;
-
-        result.push_back(json({ { "Benchmarked files", all_files },
-            { "Benchmark time(ms)", whole_time },
-            { "Average statement/ms", average_stmt_ms / (double)programs.size() },
-            { "Average line/ms", average_line_ms / (double)programs.size() } }));
-
-        std::clog << "Parse finished\n\n" << std::endl;
-
-        std::cout << result << std::endl;
-    }
-    catch (...)
-    {
-        std::clog << "Malformed json" << std::endl;
-        return 1;
     }
 
+    std::clog << "Programs: " << s.program_count << '\n'
+              << "Benchmarked files: " << s.all_files << '\n'
+              << "Analyzer crashes: " << s.parsing_crashes << '\n'
+              << "Failed program opens: " << s.failed_file_opens << '\n'
+              << "Benchmark time: " << s.whole_time << " ms" << '\n'
+              << "Average statement/ms: " << s.average_stmt_ms / (double)programs.size() << '\n'
+              << "Average line/ms: " << s.average_line_ms / (double)programs.size() << "\n\n"
+              << std::endl;
+
+    result.push_back(json({ { "Programs", s.program_count },
+        { "Benchmarked files", s.all_files },
+        { "Benchmark time(ms)", s.whole_time },
+        { "Analyzer crashes", s.parsing_crashes },
+        { "Failed program opens", s.failed_file_opens },
+        { "Average statement/ms", s.average_stmt_ms / (double)programs.size() },
+        { "Average line/ms", s.average_line_ms / (double)programs.size() } }));
+
+    std::clog << "Parse finished\n\n" << std::endl;
+
+    std::cout << result.dump(2) << std::endl;
 
     return 0;
 }
