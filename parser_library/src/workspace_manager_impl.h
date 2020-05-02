2b01bf8a7d14e49f3d1ccd3fb215cdf34f663ff1
Marcel Hruska <marcel1hruska@gmail.com>
2020-05-02 17:39:45 +0200

    remove code smell

--- parser_library/src/workspace_manager_impl.h
+++ parser_library/src/workspace_manager_impl.h
@@ -135,7 +135,7 @@ public:
         found_position = { document_uri, pos };
         if (cancel_ && *cancel_)
             return found_position;
-        ;
+        
         auto file = file_manager_.find(document_uri);
         if (dynamic_cast<workspaces::processor_file*>(file.get()) != nullptr)
             found_position = file_manager_.find_processor_file(document_uri)->get_lsp_info().go_to_definition(pos);
