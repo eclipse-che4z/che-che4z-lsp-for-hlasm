5d84a70bc341a3a6758d69deed2c7cb8388c174b
Michal Bali <michalbali32@gmail.com>
2020-06-15 17:49:36 +0200

    test: finished launch feature tests

--- language_server/test/dap/feature_launch_test.cpp
+++ language_server/test/dap/feature_launch_test.cpp
@@ -64,16 +64,15 @@ struct response_provider_mock : public response_provider
         notifs.push_back({ method, args });
         if (method == "stopped")
             stopped = true;
+        if (method == "exited")
+            exited = true;
     };
-    void respond_error(const json& id,
-        const std::string& requested_method,
-        int err_code,
-        const std::string& err_message,
-        const json& error) override {};
+    void respond_error(const json&, const std::string&, int, const std::string&, const json&) override {};
 
     std::vector<response_mock> responses;
     std::vector<notif_mock> notifs;
     std::atomic<bool> stopped = false;
+    std::atomic<bool> exited = false;
 
     void wait_for_stopped()
     {
@@ -88,6 +87,21 @@ struct response_provider_mock : public response_provider
         }
         stopped = false;
     }
+
+    void wait_for_exited()
+    {
+        size_t i = 0;
+        while (!exited)
+        {
+            if (i > 50)
+                throw std::runtime_error("Wait for exited timeout.");
+            ++i;
+
+            std::this_thread::sleep_for(100ms);
+        }
+        exited = false;
+    }
+
 
     void reset()
     {
@@ -105,7 +119,7 @@ struct feature_launch_test : public testing::Test
         feat.initialize_feature(R"({"linesStartAt1":false, "columnsStartAt1":false, "pathFormat":"path"})"_json);
 
         file_name = std::filesystem::absolute("to_trace").string();
-        file_name[0] = std::tolower(file_name[0]);
+        file_name[0] = std::tolower((char)file_name[0]);
     }
 
     void check_simple_stack_trace(json id, size_t expected_line)
@@ -128,6 +142,12 @@ struct feature_launch_test : public testing::Test
         resp_provider.reset();
     }
 
+    void wait_for_stopped()
+    {
+        resp_provider.wait_for_stopped();
+        EXPECT_EQ(resp_provider.notifs.size(), 1U);
+    }
+
     std::map<std::string, method> methods;
     response_provider_mock resp_provider;
     parser_library::workspace_manager ws_mngr;
@@ -145,8 +165,7 @@ TEST_F(feature_launch_test, stop_on_entry)
     methods["launch"]("0"_json, json { { "program", file_name }, { "stopOnEntry", true } });
     std::vector<response_mock> expected_resp = { { "0"_json, "launch", json() } };
     EXPECT_EQ(resp_provider.responses, expected_resp);
-    resp_provider.wait_for_stopped();
-    EXPECT_EQ(resp_provider.notifs.size(), 1U);
+    wait_for_stopped();
     resp_provider.reset();
 
     check_simple_stack_trace("1"_json, 0);
@@ -154,17 +173,97 @@ TEST_F(feature_launch_test, stop_on_entry)
     ws_mngr.disconnect();
 }
 
-std::string file_breakpoint = R"(  LR 1,1
-  LR 1,1
 
+std::string file_step = R"(  LR 1,1
+  MACRO
+  MAC
   LR 1,1
+  MEND
+
+  MAC
+  MAC
+
+  LR 1,1  Second breakpoint on this line
+)";
+
+TEST_F(feature_launch_test, step)
+{
+    ws_mngr.did_open_file(file_name.c_str(), 0, file_step.c_str(), file_step.size());
+
+    methods["launch"]("0"_json, json { { "program", file_name }, { "stopOnEntry", true } });
+    std::vector<response_mock> expected_resp = { { "0"_json, "launch", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+
+    check_simple_stack_trace("2"_json, 0);
+
+    methods["stepIn"]("3"_json, json());
+    expected_resp = { { "3"_json, "stepIn", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+    check_simple_stack_trace("4"_json, 1);
+
+    methods["next"]("5"_json, json());
+    expected_resp = { { "5"_json, "next", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+    check_simple_stack_trace("6"_json, 6);
+
+    methods["next"]("7"_json, json());
+    expected_resp = { { "7"_json, "next", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+    check_simple_stack_trace("8"_json, 7);
+
+    methods["stepIn"]("8"_json, json());
+    expected_resp = { { "8"_json, "stepIn", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+    methods["stackTrace"]("9"_json, json());
+    ASSERT_EQ(resp_provider.responses.size(), 1U);
+    const response_mock& r = resp_provider.responses[0];
+    EXPECT_EQ(r.id, "9"_json);
+    EXPECT_EQ(r.req_method, "stackTrace");
+    ASSERT_EQ(r.args["totalFrames"], 2U);
+    ASSERT_EQ(r.args["stackFrames"].size(), 2U);
+    const json& fs = r.args["stackFrames"];
+    size_t expected_lines[2] = { 3, 7 };
+    std::string expected_names[2] = { "MACRO", "OPENCODE" };
+    for (size_t i = 0; i < 2; ++i)
+    {
+        const json& f = fs[i];
+        EXPECT_EQ(f["name"], expected_names[i]);
+        json expected_source { { "path", file_name } };
+        EXPECT_EQ(f["source"], expected_source);
+        EXPECT_EQ(f["line"], expected_lines[i]);
+        EXPECT_EQ(f["endLine"], expected_lines[i]);
+        EXPECT_EQ(f["column"], 0);
+        EXPECT_EQ(f["endColumn"], 0);
+    }
+    resp_provider.reset();
+
+    methods["continue"]("47"_json, json());
+    resp_provider.wait_for_exited();
+}
+
+std::string file_breakpoint = R"(  LR 1,1
+  LR 1,1  First breakpoint comes on this line
+
+  LR 1,1  Second breakpoint on this line
 )";
 
 TEST_F(feature_launch_test, breakpoint)
 {
-    std::string file_name = std::filesystem::absolute("to_trace").string();
-    file_name[0] = std::tolower(file_name[0]);
-
     ws_mngr.did_open_file(file_name.c_str(), 0, file_breakpoint.c_str(), file_breakpoint.size());
 
 
@@ -181,12 +280,120 @@ TEST_F(feature_launch_test, breakpoint)
     methods["launch"]("0"_json, json { { "program", file_name }, { "stopOnEntry", false } });
     std::vector<response_mock> expected_resp = { { "0"_json, "launch", json() } };
     EXPECT_EQ(resp_provider.responses, expected_resp);
-    resp_provider.wait_for_stopped();
-    EXPECT_EQ(resp_provider.notifs.size(), 1U);
+    wait_for_stopped();
     resp_provider.reset();
 
 
     check_simple_stack_trace("2"_json, 1);
+
+    methods["continue"]("3"_json, json());
+    std::vector<response_mock> expected_resp_cont = {
+        { "3"_json, "continue", json { { "allThreadsContinued", true } } }
+    };
+    EXPECT_EQ(resp_provider.responses, expected_resp_cont);
+    wait_for_stopped();
+    resp_provider.reset();
+
+    check_simple_stack_trace("4"_json, 3);
+
+    ws_mngr.disconnect();
+}
+
+std::string file_variables = R"(&VARA SETA 4
+&VARB SETB 1
+&VARC SETC 'STH'
+      LR 1,1)";
+
+TEST_F(feature_launch_test, variables)
+{
+    ws_mngr.did_open_file(file_name.c_str(), 0, file_variables.c_str(), file_variables.size());
+
+    methods["launch"]("0"_json, json { { "program", file_name }, { "stopOnEntry", true } });
+    std::vector<response_mock> expected_resp = { { "0"_json, "launch", json() } };
+    EXPECT_EQ(resp_provider.responses, expected_resp);
+    wait_for_stopped();
+    resp_provider.reset();
+
+
+    check_simple_stack_trace("2"_json, 0);
+
+    methods["stepIn"]("3"_json, json());
+    resp_provider.wait_for_stopped();
+    methods["stepIn"]("3"_json, json());
+    resp_provider.wait_for_stopped();
+    methods["stepIn"]("3"_json, json());
+    resp_provider.wait_for_stopped();
+    resp_provider.reset();
+
+    methods["stackTrace"]("1"_json, json());
+    ASSERT_EQ(resp_provider.responses.size(), 1U);
+    response_mock r = resp_provider.responses[0];
+    EXPECT_EQ(r.id, "1"_json);
+    EXPECT_EQ(r.req_method, "stackTrace");
+    ASSERT_EQ(r.args["totalFrames"], 1U);
+    ASSERT_EQ(r.args["stackFrames"].size(), 1U);
+    json f = r.args["stackFrames"][0];
+    EXPECT_EQ(f["name"], "OPENCODE");
+    json expected_source { { "path", file_name } };
+    EXPECT_EQ(f["source"], expected_source);
+    EXPECT_EQ(f["line"], 3U);
+    EXPECT_EQ(f["endLine"], 3U);
+    EXPECT_EQ(f["column"], 0);
+    EXPECT_EQ(f["endColumn"], 0);
+    json frame_id = f["id"];
+    resp_provider.reset();
+
+    methods["scopes"]("5"_json, json { { "frameId", frame_id } });
+    ASSERT_EQ(resp_provider.responses.size(), 1U);
+    r = resp_provider.responses[0];
+    EXPECT_EQ(r.id, "5"_json);
+    EXPECT_EQ(r.req_method, "scopes");
+    json scopes = r.args["scopes"];
+    json locals_ref;
+    for (const json & scope : scopes)
+    {
+        EXPECT_FALSE(scope.find("name") == scope.end());
+        EXPECT_FALSE(scope.find("variablesReference") == scope.end());
+        EXPECT_EQ(scope["expensive"], json(false));
+        EXPECT_EQ(scope["source"], expected_source);
+        if (scope["name"] == "Locals")
+            locals_ref = scope["variablesReference"];
+    }
+    resp_provider.reset();
+
+    methods["variables"]("8"_json, json { { "variablesReference", locals_ref } });
+    ASSERT_EQ(resp_provider.responses.size(), 1U);
+    r = resp_provider.responses[0];
+    EXPECT_EQ(r.id, "8"_json);
+    EXPECT_EQ(r.req_method, "variables");
+    const json & variables = r.args["variables"];
+    size_t var_count = 0;
+    for (const json & var : variables)
+    {
+        EXPECT_FALSE(var.find("name") == var.end());
+        EXPECT_FALSE(var.find("value") == var.end());
+        EXPECT_FALSE(var.find("variablesReference") == var.end());
+        if (var["name"] == "VARA")
+        {
+            EXPECT_EQ(var["value"], json("4"));
+            EXPECT_EQ(var["type"], json("A_TYPE"));
+            ++var_count;
+        }
+        else if (var["name"] == "VARB")
+        {
+            EXPECT_EQ(var["value"], json("TRUE"));
+            EXPECT_EQ(var["type"], json("B_TYPE"));
+            ++var_count;
+        }
+        else if (var["name"] == "VARC")
+        {
+            EXPECT_EQ(var["value"], json("STH"));
+            EXPECT_EQ(var["type"], json("C_TYPE"));
+            ++var_count;
+        }
+    }
+    //test, that all variable symbols were reported
+    EXPECT_EQ(var_count, 3);
 
     ws_mngr.disconnect();
 }
