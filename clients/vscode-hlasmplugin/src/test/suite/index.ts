3201fa025e5ef9dd0b117ab544af8869fdd9ae63
Marcel Hruska <marcel1hruska@gmail.com>
2020-06-17 12:53:28 +0200

    suite change to start integration tests for theia correctly

--- clients/vscode-hlasmplugin/src/test/suite/index.ts
+++ clients/vscode-hlasmplugin/src/test/suite/index.ts
@@ -16,18 +16,11 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 import * as vscode from 'vscode';
+import * as process from 'process';
 
 export async function run(): Promise<void> {
 	const sourceRoot = path.join(__dirname, '..', '..');
-
-	// decache files on windows to be hookable by nyc
-	let decache = require("decache");
-	glob.sync("**/**.js", {
-		cwd: sourceRoot
-	}).forEach(file => {
-		decache(path.join(sourceRoot, file));
-	});
-		
+	
 	// initialize nyc code coverage
 	const NYC = require('nyc');
 	const nyc = new NYC({ 
@@ -37,15 +30,27 @@ export async function run(): Promise<void> {
 		exclude: ['**/test/**','.vscode-test/**']
 	});
 
-	nyc.createTempDirectory();
-	nyc.wrap();
+	const is_vscode = process.execPath.includes('Code');
+	// only on VSCode
+	if (is_vscode) {
+		// decache files on windows to be hookable by nyc
+		let decache = require("decache");
+		glob.sync("**/**.js", {
+			cwd: sourceRoot
+		}).forEach(file => {
+			decache(path.join(sourceRoot, file));
+		});
+
+		nyc.createTempDirectory();
+		nyc.wrap();
+	}
 
 	// Create the mocha test
 	const mocha = new Mocha({ ui: 'tdd', color: true });
 	const testsPath = path.join(__dirname, '..');
 
 	await new Promise((resolve, reject) => {
-		glob('**/**.test.js', { cwd: testsPath }, (_, files) => {
+		glob((is_vscode) ? '**/**.test.js' : '**/integration.test.js', { cwd: testsPath }, (_, files) => {
 				// Add files to the test suite
 				files.forEach(file => 
 					mocha.addFile(path.resolve(testsPath, file)));
@@ -68,8 +73,11 @@ export async function run(): Promise<void> {
 		});
 	});
 
-	// report code coverage
-	nyc.writeCoverageFile();
-	nyc.report();
+	if (is_vscode) {
+		// report code coverage
+		nyc.writeCoverageFile();
+		nyc.report();
+	}
+
 	console.log('Report created');
 }
