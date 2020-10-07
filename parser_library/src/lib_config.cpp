cdad285b48f756a9ff5e37586afa0dcbf7a82c03
Michal Bali <michalbali32@gmail.com>
2020-10-07 11:28:13 +0200

    license headers

--- parser_library/src/lib_config.cpp
+++ parser_library/src/lib_config.cpp
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2019 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
 #include "lib_config.h"
 
 static std::shared_ptr<lib_config> config_instance = std::make_shared<lib_config>();
