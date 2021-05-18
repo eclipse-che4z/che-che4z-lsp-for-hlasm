330d4e524e181470a5a33d7a6511cbfa34497907
Sweta Shah <swetas04@GMAIL.COM>
2021-05-18 09:22:43 +0200

    Cleanup

--- parser_library/src/expressions/mach_operator.h
+++ parser_library/src/expressions/mach_operator.h
@@ -149,7 +149,7 @@ inline mach_expression::value_t mach_expr_binary<rel_addr>::evaluate(mach_evalua
     auto target = right_->evaluate(info);
     if (target.value_kind() == context::symbol_value_kind::ABS)
     {
-        add_diagnostic(diagnostic_op::warn_D031(get_range(), std::to_string(right_->evaluate(info).get_abs())));
+        add_diagnostic(diagnostic_op::warn_D031(get_range(), std::to_string(target.get_abs())));
         return target;
     }
     return target - location;
