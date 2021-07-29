01058ebb60a2d23c409b7070d5b9b1f0e06a8667
Slavomir Kucera <slavomir.kucera@broadcom.com>
2021-07-29 18:16:52 +0200

    replace id set by a simple structure

--- parser_library/src/context/id_storage.h
+++ parser_library/src/context/id_storage.h
@@ -74,7 +74,34 @@ public:
 
 using id_index = id_storage::const_pointer;
 
-using id_set = std::unordered_set<id_index>;
+class id_set : std::vector<id_index>
+{
+    using base = std::vector<id_index>;
+
+public:
+    void emplace(id_index id)
+    {
+        auto i = std::lower_bound(base::begin(), base::end(), id);
+        if (i == base::end() || *i != id)
+            insert(i, id);
+    }
+
+    auto begin() const { return base::begin(); }
+    auto end() const { return base::end(); }
+    auto find(id_index id) const { return std::lower_bound(begin(), end(), id); }
+
+    using base::empty;
+    using base::size;
+
+    using base::clear;
+
+    void erase(id_index id)
+    {
+        auto i = std::lower_bound(base::begin(), base::end(), id);
+        if (i != base::end() && *i == id)
+            base::erase(i);
+    }
+};
 
 } // namespace hlasm_plugin::parser_library::context
 
