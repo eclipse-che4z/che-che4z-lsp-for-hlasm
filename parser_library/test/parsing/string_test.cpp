2cdf487b89cb9e4b47f93d89cf5dc2bcaab39c4e
Michal Bali <michalbali32@gmail.com>
2021-08-06 13:08:23 +0200

    tests for macro param apostrophe parsing

--- parser_library/test/parsing/string_test.cpp
+++ parser_library/test/parsing/string_test.cpp
@@ -43,3 +43,61 @@ TEST(parser, ca_string_double_ampersand)
 
     ASSERT_EQ(ctx.get_var_sym(it)->access_set_symbol_base()->access_set_symbol<context::C_t>()->get_value(), "&&");
 }
+
+namespace {
+
+struct test_param
+{
+    std::string name;
+    std::string parameter;
+    std::string expected;
+};
+
+struct stringer
+{
+    std::string operator()(::testing::TestParamInfo<test_param> p) { return p.param.name; }
+};
+
+class parser_string_fixture : public ::testing::TestWithParam<test_param>
+{};
+
+INSTANTIATE_TEST_SUITE_P(parser,
+    parser_string_fixture,
+    ::testing::Values(test_param { "A_no_attr", "A'SYM 93'", "A'SYM 93'" },
+        test_param { "D_attr", "D'SYM 93'", "D'SYM 93'" },
+        test_param { "I_attr", "I'SYM 93'", "I'SYM" },
+        test_param { "K_attr", "K'SYM 93'", "K'SYM 93'" },
+        test_param { "L_attr", "L'SYM 93'", "L'SYM" },
+        test_param { "N_attr", "N'SYM 93'", "N'SYM 93'" },
+        test_param { "O_attr", "O'SYM 93'", "O'SYM" },
+        test_param { "S_attr", "S'SYM 93'", "S'SYM" },
+        test_param { "T_attr", "T'SYM 93'", "T'SYM" },
+        test_param { "attr_and_string", "S'SYM' STH'", "S'SYM' STH'" },
+        test_param { "literal_FD", "=FD'SYM STH'", "=FD'SYM STH'" },
+        test_param { "literal_FS", "=FS'SYM STH'", "=FS'SYM STH'" },
+        test_param { "quote_before_attr_L", "\"L'SYM 93'", "\"L'SYM" },
+        test_param { "quote_before_attr_D", "\"D'SYM 93'", "\"D'SYM 93'" }),
+    stringer());
+} // namespace
+
+TEST_P(parser_string_fixture, basic)
+{
+    std::string input = R"(
+ GBLC &PAR
+ MACRO
+ MAC &VAR
+ GBLC &PAR
+&PAR SETC &VAR 
+ MEND
+ 
+ MAC )" + GetParam().parameter;
+    analyzer a(input);
+    a.analyze();
+
+    a.collect_diags();
+    EXPECT_EQ(a.diags().size(), 0U);
+
+    auto par_value = get_var_value<C_t>(a.hlasm_ctx(), "PAR");
+    ASSERT_TRUE(par_value.has_value());
+    EXPECT_EQ(*par_value, GetParam().expected);
+}
