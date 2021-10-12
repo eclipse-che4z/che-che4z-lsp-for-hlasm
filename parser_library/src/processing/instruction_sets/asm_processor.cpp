17e5f207c21317eff3405095c87ba6f265808a0a
Sweta Shah <swetas04@GMAIL.COM>
2021-10-12 15:16:10 +0200

    Revert "End stopped due to error removed"
    This reverts commit 1f77282fedc70586e168a6e19bae8819b788605c.

--- parser_library/src/processing/instruction_sets/asm_processor.cpp
+++ parser_library/src/processing/instruction_sets/asm_processor.cpp
@@ -822,8 +822,8 @@ void asm_processor::process_END(rebuilt_statement stmt)
 {
     const auto& label = stmt.label_ref();
 
-    check(stmt, hlasm_ctx, checker_, *this);
-
+    if (!check(stmt, hlasm_ctx, checker_, *this))
+        return;
     if (!(label.type == semantics::label_si_type::EMPTY || label.type == semantics::label_si_type::SEQ))
     {
         add_diagnostic(diagnostic_op::warning_A249_sequence_symbol_expected(stmt.label_ref().field_range));
@@ -840,6 +840,7 @@ void asm_processor::process_END(rebuilt_statement stmt)
             {
                 add_diagnostic(
                     diagnostic_op::error_E032(std::to_string(symbol.get_abs()), stmt.operands_ref().field_range));
+                return;
             }
         }
     }
