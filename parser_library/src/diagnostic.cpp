64f49a3bbae5825d74e0e1a74a4c652712c424a9
Sweta Shah <swetas04@GMAIL.COM>
2021-10-12 09:58:23 +0200

    Update parser_library/src/diagnostic.cpp
    Co-authored-by: slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>

--- parser_library/src/diagnostic.cpp
+++ parser_library/src/diagnostic.cpp
@@ -1883,7 +1883,7 @@ diagnostic_op diagnostic_op::warning_W014(const range& range)
 diagnostic_op diagnostic_op::warning_W015(const range& range)
 {
     return diagnostic_op(
-        diagnostic_severity::warning, "W015", "End of Source Input Reached,batch mode is not yet supported", range);
+        diagnostic_severity::warning, "W015", "End of source input reached, batch mode is not supported yet", range);
 }
 diagnostic_op diagnostic_op::error_EQU1(const range& range)
 {
