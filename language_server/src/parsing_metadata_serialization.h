97e1bdebb9a1854028de043fcb225cd50fde5336
Michal Bali <michalbali32@gmail.com>
2021-11-05 13:39:35 +0100

    write the error name directly to method name in telemetry event

--- language_server/src/parsing_metadata_serialization.h
+++ language_server/src/parsing_metadata_serialization.h
@@ -71,10 +71,11 @@ void inline to_json(nlohmann::json& j, const telemetry_info& info)
 void inline to_json(nlohmann::json& j, const telemetry_error& err)
 {
     nlohmann::json properties;
-    properties["error_type"] = err.error_type;
     if (err.error_details != "")
         properties["error_details"] = err.error_details;
-    j = nlohmann::json { { "method_name", "server_error" }, { "properties", properties }, { "measurements", {} } };
+    j = nlohmann::json {
+        { "method_name", "server_error/" + err.error_type }, { "properties", properties }, { "measurements", {} }
+    };
 }
 
 void inline to_json(nlohmann::json& j, const telemetry_message& message)
