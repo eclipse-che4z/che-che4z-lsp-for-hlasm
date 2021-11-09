6a9723f35b0ad7269c35775c0ecc871b8ad60048
Slavomir Kucera <slavomir.kucera@broadcom.com>
2021-11-09 12:43:10 +0100

    tests

---
+++ parser_library/test/parsing/deferred_statement_test.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2021 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+#include "gtest/gtest.h"
+
+#include "../common_testing.h"
+
+
+TEST(deferred_statement, split_var)
+{
+    std::string input = R"(
+   MACRO
+   MAC &AA
+   &AA C'                                                            &AX
+               A'
+   MEND
+
+   MAC DC
+)";
+    analyzer a(input);
+    a.analyze();
+
+    a.collect_diags();
+    EXPECT_TRUE(a.diags().empty());
+
+    auto aa1 = a.context().lsp_ctx->hover("", { 3, 71 });
+    auto aa2 = a.context().lsp_ctx->hover("", { 4, 16 });
+    EXPECT_EQ(aa1, "MACRO parameter");
+    EXPECT_EQ(aa2, "MACRO parameter");
+}
+
+TEST(deferred_statement, nested_macro_def_with_continuation)
+{
+    std::string input = R"(
+ MACRO
+ MAC
+
+ MACRO
+ MAC2                                                                  X
+               &OP=,                                                   X
+               &OP2=
+
+ MEND
+
+ MEND
+
+ MAC
+
+ MAC2 OP=A,OP2=B
+)";
+    analyzer a(input);
+    a.analyze();
+
+    a.collect_diags();
+    EXPECT_TRUE(a.diags().empty());
+}
