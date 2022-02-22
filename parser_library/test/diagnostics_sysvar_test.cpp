859107489f3bb6386ce0770cad0f4595b957260a
jirimosinger <jiri.mosinger@broadcom.com>
2022-02-22 10:13:53 +0100

    Added diag tests for SYSDATC, SYSECT, SYSLOC and SYSOPT_RENT

--- parser_library/test/diagnostics_sysvar_test.cpp
+++ parser_library/test/diagnostics_sysvar_test.cpp
@@ -41,98 +41,99 @@ struct diagnostics_sysvar_params
     }
 };
 
-class diagnostics_sysvar_without_subscript_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
+class diagnostics_sysvar_invalid_opcode_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
 {};
 
-class diagnostics_sysvar_with_subscript_1_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
+class diagnostics_sysvar_null_opcode_invalid_subscript_fixture
+    : public ::testing::TestWithParam<diagnostics_sysvar_params>
 {};
 
-class diagnostics_sysvar_with_subscripts_1_1_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_1_1_1_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscript_0_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_1_0_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_1_1_0_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscript_2_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_2_0_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_2_1_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
-{};
-
-class diagnostics_sysvar_with_subscripts_2_2_2_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
+class diagnostics_sysvar_null_opcode_fixture : public ::testing::TestWithParam<diagnostics_sysvar_params>
 {};
 
 } // namespace
 
 INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_without_subscript_fixture,
+    diagnostics_sysvar_invalid_opcode_fixture,
+    ::testing::Values(diagnostics_sysvar_params::create_input("SYSDATC", ""),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(1,1,1)"),
+        diagnostics_sysvar_params::create_input("SYSECT", ""),
+        diagnostics_sysvar_params::create_input("SYSECT", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(1,1,1)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", ""),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(1,1,1)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", ""),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(1,1,1)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", ""),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1,1)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", ""),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(1)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(1,1)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(1,1,1)")));
+
+INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
+    diagnostics_sysvar_null_opcode_invalid_subscript_fixture,
     ::testing::Values(
-        diagnostics_sysvar_params::create_input("SYSNDX", ""), diagnostics_sysvar_params::create_input("SYSNEST", "")));
+        diagnostics_sysvar_params::create_input("SYSDATC", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(1,1,0)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(1,1,0)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(1,1,0)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(1,1,0)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1,0)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(0)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(1,0)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(1,1,0)")));
 
 INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscript_1_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(1)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(1)")));
+    diagnostics_sysvar_null_opcode_fixture,
+    ::testing::Values(
+        diagnostics_sysvar_params::create_input("SYSDATC", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSDATC", "(2,2,2)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSECT", "(2,2,2)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSLOC", "(2,2,2)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSNDX", "(2,2,2)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSNEST", "(2,2,2)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(2)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(2,0)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(2,1)"),
+        diagnostics_sysvar_params::create_input("SYSOPT_RENT", "(2,2,2)")
+        ));
 
 
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_1_1_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(1,1)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1)")));
 
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_1_1_1_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(1,1,1)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1,1)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscript_0_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(0)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(0)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_1_0_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(1,0)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(1,0)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_1_1_0_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(1,1,0)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(1,1,0)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscript_2_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(2)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(2)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_2_0_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(2,0)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(2,0)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_2_1_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(2,1)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(2,1)")));
-
-INSTANTIATE_TEST_SUITE_P(diagnostics_sysvar,
-    diagnostics_sysvar_with_subscripts_2_2_2_fixture,
-    ::testing::Values(diagnostics_sysvar_params::create_input("SYSNDX", "(2,2,2)"),
-        diagnostics_sysvar_params::create_input("SYSNEST", "(2,2,2)")));
-
-TEST_P(diagnostics_sysvar_without_subscript_fixture, invalid_opcode)
+TEST_P(diagnostics_sysvar_invalid_opcode_fixture, invalid_opcode)
 {
     std::string input(GetParam().input);
 
@@ -144,50 +145,7 @@ TEST_P(diagnostics_sysvar_without_subscript_fixture, invalid_opcode)
     EXPECT_TRUE(matches_message_codes(a.diags(), { "E049" }));
 }
 
-
-
-TEST_P(diagnostics_sysvar_with_subscript_1_fixture, invalid_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E049" }));
-}
-
-
-TEST_P(diagnostics_sysvar_with_subscripts_1_1_fixture, invalid_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E049" }));
-}
-
-
-
-TEST_P(diagnostics_sysvar_with_subscripts_1_1_1_fixture, invalid_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E049" }));
-}
-
-
-
-TEST_P(diagnostics_sysvar_with_subscript_0_fixture, null_opcode_invalid_subscript)
+TEST_P(diagnostics_sysvar_null_opcode_invalid_subscript_fixture, null_opcode_invalid_subscript)
 {
     std::string input(GetParam().input);
 
@@ -199,71 +157,7 @@ TEST_P(diagnostics_sysvar_with_subscript_0_fixture, null_opcode_invalid_subscrip
     EXPECT_TRUE(matches_message_codes(a.diags(), { "E012", "E074" }));
 }
 
-
-
-TEST_P(diagnostics_sysvar_with_subscripts_1_0_fixture, null_opcode_invalid_subscript)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)2);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E012", "E074" }));
-}
-
-TEST_P(diagnostics_sysvar_with_subscripts_1_1_0_fixture, null_opcode_invalid_subscript)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)2);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E012", "E074" }));
-}
-
-TEST_P(diagnostics_sysvar_with_subscript_2_fixture, null_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E074" }));
-}
-
-
-
-TEST_P(diagnostics_sysvar_with_subscripts_2_0_fixture, null_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E074" }));
-}
-
-TEST_P(diagnostics_sysvar_with_subscripts_2_1_fixture, null_opcode)
-{
-    std::string input(GetParam().input);
-
-    analyzer a(input);
-    a.analyze();
-    a.collect_diags();
-
-    ASSERT_EQ(a.diags().size(), (size_t)1);
-    EXPECT_TRUE(matches_message_codes(a.diags(), { "E074" }));
-}
-
-TEST_P(diagnostics_sysvar_with_subscripts_2_2_2_fixture, null_opcode)
+TEST_P(diagnostics_sysvar_null_opcode_fixture, null_opcode)
 {
     std::string input(GetParam().input);
 
