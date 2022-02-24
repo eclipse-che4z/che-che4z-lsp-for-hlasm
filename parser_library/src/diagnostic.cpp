d7f4e08fa8d99a039ddb6e3f5c3e7e92a7419e08
jirimosinger <jiri.mosinger@broadcom.com>
2022-02-24 11:41:02 +0100

    Added SYSLIST related diagnostics message

--- parser_library/src/diagnostic.cpp
+++ parser_library/src/diagnostic.cpp
@@ -1902,11 +1902,19 @@ diagnostic_op diagnostic_op::warning_W014(const range& range)
         "Undefined keyword parameter, default to positional including keyword",
         range);
 }
+
 diagnostic_op diagnostic_op::warning_W015(const range& range)
 {
     return diagnostic_op(
         diagnostic_severity::warning, "W015", "End of source input reached, batch mode is not supported yet", range);
 }
+
+diagnostic_op diagnostic_op::warning_W016(const range& range)
+{
+    return diagnostic_op(
+        diagnostic_severity::warning, "W016", "Unsubscripted SYSLIST; default=SYSLIST(1)", range);
+}
+
 diagnostic_op diagnostic_op::error_EQU1(const range& range)
 {
     return diagnostic_op(diagnostic_severity::error, "EQU1", "Constant redefinition", range);
