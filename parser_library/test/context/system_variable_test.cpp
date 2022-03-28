de79b8750c0ac19bf95bf0758e086cc963bfb390
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-03-28 09:46:44 +0200

    Added tests for more system variables

--- parser_library/test/context/system_variable_test.cpp
+++ parser_library/test/context/system_variable_test.cpp
@@ -18,88 +18,59 @@
 #include "../mock_parse_lib_provider.h"
 
 namespace {
-
-std::vector<std::string> subscripts_default = { "", "(1)", "(1,1)", "(1,1,1)", "(2)", "(2,2)", "(3)" };
-std::vector<std::string> subscripts_sysmac = { "", "(0)", "(1)", "(1,1)", "(1,1,1)", "(2)", "(2,2)", "(3)" };
-std::vector<std::string> subscripts_syslist = { "",
-    "(0)",
-    "(1)",
-    "(1,0)",
-    "(1,1)",
-    "(1,1,1)",
-    "(2)",
-    "(2,0)",
-    "(2,2)",
-    "(3)",
-    "(3,1)",
-    "(3,1,1)",
-    "(3,2)",
-    "(3,3)",
-    "(3,4)",
-    "(3,4,1)",
-    "(3,5)",
-    "(3,5,1)",
-    "(3,5,1,1)",
-    "(3,5,1,2)",
-    "(3,5,2)",
-    "(3,5,2,1)",
-    "(3,5,2,1)",
-    "(4)",
-    "(5)",
-    "(5,0)",
-    "(5,1)",
-    "(7)",
-    "(7,0)",
-    "(7,1)",
-    "(7,1,2)",
-    "(7,2)",
-    "(10)",
-    "(14)",
-    "(20)" };
-
 struct system_variable_params
 {
     std::string input;
     std::vector<std::string> exp_results;
 
-    static system_variable_params create_input(std::string system_variable,
-        std::vector<std::string> expected_results,
-        std::vector<std::string>& subscripts = subscripts_default)
+    static system_variable_params create_input(std::string system_variable, std::vector<std::string> expected_results)
     {
-        assert(subscripts.size() == expected_results.size());
+        assert(5 == expected_results.size());
 
         system_variable_params params;
         params.input = R"(
-      MACRO
-      MAC
-)";
+          MACRO
+          NESTED
+          GBLC &VAR2
+NSTSCTN   RSECT
+&VAR2     SETC '&)"
+            + system_variable + R"('
+          MEND
+          
+          MACRO
+          MAC_1
+          GBLC &VAR1,&VAR3
+MAC1SCTN  DSECT
+&VAR1     SETC '&)"
+            + system_variable + R"('
+          NESTED
+&VAR3     SETC '&)"
+            + system_variable + R"('
+          MEND
 
-        for (size_t i = 0; i < subscripts.size(); ++i)
-        {
-            params.input += R"(  GBLC A)" + std::to_string(i) + R"(
-)";
-        }
+          MACRO
+          MAC_2
+          GBLC &VAR4
+MAC2SCTN  COM
+&VAR4     SETC '&)"
+            + system_variable + R"('
+          MEND
 
-        for (size_t i = 0; i < subscripts.size(); ++i)
-        {
-            params.input += R"(&A)" + std::to_string(i) + R"( SETC  '&)" + system_variable + subscripts[i] + R"('
-)";
-        }
-
-        params.input += R"(
-        MEND
-
-TEST CSECT
-)";
-
-        for (size_t i = 0; i < subscripts.size(); ++i)
-        {
-            params.input += R"(  GBLC A)" + std::to_string(i) + R"(
-)";
-        }
-
-        params.input += R"(
-NNN   MAC WW,XX,(A,,B,,(AA,,BB,CC,,DD),8,,()),,YY,,(),,,ZZ
+          MACRO
+          MAC_3
+          GBLC &VAR5
+MAC3SCTN  CSECT
+&VAR5     SETC '&)"
+            + system_variable + R"('
+          MEND
+          
+          GBLC &VAR1,&VAR2,&VAR3,&VAR4,&VAR5
+MAIN      START
+          MAC_1
+LOC       LOCTR ,
+          MAC_2
+          MAC_3
+          END
 )";
 
         params.exp_results = std::move(expected_results);
@@ -110,60 +81,17 @@ NNN   MAC WW,XX,(A,,B,,(AA,,BB,CC,,DD),8,,()),,YY,,(),,,ZZ
 
 class system_variable_standard_behavior_fixture : public ::testing::TestWithParam<system_variable_params>
 {};
-
 } // namespace
 
 INSTANTIATE_TEST_SUITE_P(system_variable,
     system_variable_standard_behavior_fixture,
-    ::testing::Values(system_variable_params::create_input("SYSECT", { "TEST", "TEST", "TEST", "TEST", "", "", "" }),
-        system_variable_params::create_input("SYSLOC", { "TEST", "TEST", "TEST", "TEST", "", "", "" }),
-        system_variable_params::create_input(
-            "SYSMAC", { "MAC", "MAC", "OPEN CODE", "OPEN CODE", "OPEN CODE", "", "", "" }, subscripts_sysmac),
-        system_variable_params::create_input("SYSNDX", { "0001", "0001", "0001", "0001", "", "", "" }),
-        system_variable_params::create_input("SYSNEST", { "1", "1", "1", "1", "", "", "" }),
-        system_variable_params::create_input("SYSOPT_RENT", { "0", "0", "0", "0", "", "", "" }),
-        system_variable_params::create_input("SYSPARM", { "PAR", "PAR", "PAR", "PAR", "", "", "" }),
-        system_variable_params::create_input("SYSSTMT", { "00000038", "00000039", "00000040", "00000041", "", "", "" }),
-        system_variable_params::create_input("SYSSTYP", { "CSECT", "CSECT", "CSECT", "CSECT", "", "", "" }),
-        system_variable_params::create_input(
-            "SYSTEM_ID", { "z/OS 02.04.00", "z/OS 02.04.00", "z/OS 02.04.00", "z/OS 02.04.00", "", "", "" }),
-        system_variable_params::create_input("SYSLIST",
-            { "WW",
-                "NNN",
-                "WW",
-                "",
-                "WW",
-                "WW",
-                "XX",
-                "",
-                "",
-                "(A,,B,,(AA,,BB,CC,,DD),8,,())",
-                "A",
-                "A",
-                "",
-                "B",
-                "",
-                "",
-                "(AA,,BB,CC,,DD)",
-                "AA",
-                "AA",
-                "",
-                "",
-                "",
-                "",
-                "",
-                "YY",
-                "",
-                "YY",
-                "()",
-                "",
-                "",
-                "",
-                "",
-                "ZZ",
-                "",
-                "" },
-            subscripts_syslist)));
+    ::testing::Values(
+        system_variable_params::create_input("SYSECT", { "MAIN", "MAC1SCTN", "MAIN", "NSTSCTN", "MAC2SCTN" }),
+        system_variable_params::create_input("SYSLOC", { "MAIN", "MAC1SCTN", "MAIN", "LOC", "MAC2SCTN" }),
+        system_variable_params::create_input("SYSMAC", { "MAC_1", "NESTED", "MAC_1", "MAC_2", "MAC_3" }),
+        system_variable_params::create_input("SYSNDX", { "0001", "0002", "0001", "0003", "0004" }),
+        system_variable_params::create_input("SYSNEST", { "1", "2", "1", "1", "1" }),
+        system_variable_params::create_input("SYSSTYP", { "CSECT", "DSECT", "CSECT", "RSECT", "COM" })));
 
 TEST_P(system_variable_standard_behavior_fixture, standard_behavior)
 {
@@ -176,7 +104,7 @@ TEST_P(system_variable_standard_behavior_fixture, standard_behavior)
 
     for (size_t i = 0; i < exp_behavior.size(); ++i)
     {
-        EXPECT_EQ(get_var_value<context::C_t>(a.hlasm_ctx(), "A" + std::to_string(i)), exp_behavior[i])
+        EXPECT_EQ(get_var_value<context::C_t>(a.hlasm_ctx(), "VAR" + std::to_string(i + 1)), exp_behavior[i])
             << "for i = " << i;
     }
 }
@@ -283,4 +211,49 @@ TEST(system_variable, sysstmt_aread)
     EXPECT_EQ(a.diags().size(), (size_t)0);
 
     EXPECT_EQ(get_var_value<A_t>(a.hlasm_ctx(), "A"), 13);
+}
+
+TEST(system_variable, sysstyp_csect)
+{
+    std::string input = R"(
+        MACRO
+        MAC
+        GBLC &VAR
+&VAR    SETC '&SYSSTYP'
+        MEND
+        
+        GBLC &VAR
+MAIN    CSECT
+        MAC
+        END
+)";
+
+    analyzer a(input);
+    a.analyze();
+    a.collect_diags();
+    EXPECT_EQ(a.diags().size(), (size_t)0);
+
+    EXPECT_EQ(get_var_value<C_t>(a.hlasm_ctx(), "VAR"), "CSECT");
+}
+
+TEST(system_variable, sysstyp_empty)
+{
+    std::string input = R"(
+        MACRO
+        MAC
+        GBLC &VAR
+&VAR    SETC '&SYSSTYP'
+        MEND
+        
+        GBLC &VAR
+        MAC
+        END
+)";
+
+    analyzer a(input);
+    a.analyze();
+    a.collect_diags();
+    EXPECT_EQ(a.diags().size(), (size_t)0);
+
+    EXPECT_EQ(get_var_value<C_t>(a.hlasm_ctx(), "VAR"), "");
 }
