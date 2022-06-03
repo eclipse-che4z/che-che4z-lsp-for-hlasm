f95a0d6294d604f9d9e45528005d5b0898a44c32
Slavomir Kucera <slavomir.kucera@broadcom.com>
2022-06-03 15:45:17 +0200

    Enhance continuation insertion command

--- clients/vscode-hlasmplugin/src/continuationHandler.ts
+++ clients/vscode-hlasmplugin/src/continuationHandler.ts
@@ -23,35 +23,92 @@ import { getConfig } from './eventsHandler'
 export class ContinuationHandler {
     dispose() { }
 
+    private extractContinuationSymbolSingleLine(document: vscode.TextDocument, line: number, continuationOffset: number): string {
+        if (line >= 0 && line < document.lineCount) {
+            const lineText = document.lineAt(line).text;
+            if (continuationOffset < lineText.length) {
+                const character = lineText.at(continuationOffset);
+                if (character != " ")
+                    return character;
+            }
+        }
+        return "";
+    }
+
+    private extractMostCommonContinuationSymbol(document: vscode.TextDocument, continuationOffset: number): string {
+        let continuationSymbols: { [name: string]: number } = {};
+        for (let i = 0; i < Math.min(document.lineCount, 5000); ++i) {
+            const line = document.lineAt(i).text;
+            if (line.length <= continuationOffset)
+                continue;
+            const candidate = line.at(continuationOffset);
+            if (candidate == ' ')
+                continue;
+            continuationSymbols[candidate] = (continuationSymbols[candidate] || 0) + 1;
+        }
+        return Object.entries(continuationSymbols).reduce((prev, current) => prev[1] >= current[1] ? prev : current, ["X", 0])[0];
+    }
+
+    private extractContinuationSymbol(document: vscode.TextDocument, line: number, continuationOffset: number): string {
+        return this.extractContinuationSymbolSingleLine(document, line, continuationOffset) ||
+            this.extractContinuationSymbolSingleLine(document, line - 1, continuationOffset) ||
+            this.extractContinuationSymbolSingleLine(document, line + 1, continuationOffset) ||
+            this.extractMostCommonContinuationSymbol(document, continuationOffset);
+    }
+
     // insert continuation character X to the current line
     insertContinuation(editor: vscode.TextEditor, edit: vscode.TextEditorEdit, continuationOffset: number, continueColumn: number) {
         const sel = editor.selection;
 
         // retrieve continuation information
-        const isContinued = isLineContinued(editor.document, sel.active.line, continuationOffset);
+        const line = sel.active.line;
+        const col = sel.active.character;
+        const isContinued = isLineContinued(editor.document, line, continuationOffset);
+        const doc = editor.document;
+        const lineText = doc.lineAt(line).text;
+        const eol = doc.eol == vscode.EndOfLine.LF ? '\n' : '\r\n';
 
+        let contSymbol = this.extractContinuationSymbol(doc, line, continuationOffset);
         if (!isContinued) {
-            if (sel.active.character < continuationOffset) {
-                const lastChar =
-                    (sel.active.character < continuationOffset)
-                        ? editor.document.lineAt(sel.active).text.length
-                        : continuationOffset;
-
-                edit.insert(
-                    new vscode.Position(sel.active.line, lastChar),
-                    " ".repeat(continuationOffset - lastChar));
+            let initialLineSize = editor.document.lineAt(line).text.length;
+            if (initialLineSize <= continuationOffset) {
+                edit.insert(new vscode.Position(line, col),
+                    ' '.repeat(continuationOffset - col) + contSymbol + eol +
+                    ' '.repeat(continueColumn));
             }
-            const continuationPosition = new vscode.Position(sel.active.line, continuationOffset);
-            // see https://github.com/microsoft/vscode/issues/32058 why replace does not work
-            edit.delete(new vscode.Range(continuationPosition, new vscode.Position(sel.active.line, continuationOffset + 1)));
-            edit.insert(continuationPosition, 'X\r\n' + ' '.repeat(continueColumn));
+            else {
+                let reinsert = '';
+                let ignoredPart = '';
+                if (col < continuationOffset)
+                    reinsert = lineText.substring(col, continuationOffset).trimEnd();
+
+                ignoredPart = lineText.substring(continuationOffset + 1);
+                // see https://github.com/microsoft/vscode/issues/32058 why replace does not work
+                const insertionPoint = new vscode.Position(line, Math.min(col, continuationOffset));
+                edit.delete(new vscode.Range(insertionPoint, new vscode.Position(line, lineText.length)));
+                edit.insert(insertionPoint,
+                    ' '.repeat(Math.max(continuationOffset - col, 0)) + contSymbol + ignoredPart + eol +
+                    ' '.repeat(continueColumn) + reinsert);
+            }
         }
         // add extra continuation on already continued line
-        else
-            edit.insert(new vscode.Position(
-                editor.selection.active.line,
-                editor.document.lineAt(editor.selection.active).text.length),
-                "\r\n" + " ".repeat(continuationOffset) + "X");
+        else {
+            let prefix = '';
+            let reinsert = '';
+            if (col < continuationOffset) {
+                reinsert = lineText.substring(col, continuationOffset).trimEnd();
+                prefix = ' '.repeat(continuationOffset - col) + lineText.substring(continuationOffset, lineText.length);
+            }
+            else {
+                prefix = lineText.substring(col, lineText.length);
+            }
+            const insertionPoint = new vscode.Position(line, col);
+            edit.delete(new vscode.Range(insertionPoint, new vscode.Position(line, lineText.length)));
+            edit.insert(insertionPoint,
+                prefix + eol +
+                ' '.repeat(continueColumn) + reinsert + ' '.repeat(continuationOffset - continueColumn - reinsert.length) + contSymbol);
+            setImmediate(() => setCursor(editor, new vscode.Position(line + 1, continueColumn + reinsert.length)));
+        }
     }
 
     // remove continuation from previous line
