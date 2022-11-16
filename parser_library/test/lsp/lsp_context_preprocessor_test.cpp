bb8d29bd375303116d6a7272c48ee6892f46375a
Jiri Mosinger <jiri.mosinger@broadcom.com>
2022-11-16 15:12:08 +0100

    Variable rename

--- parser_library/test/lsp/lsp_context_preprocessor_test.cpp
+++ parser_library/test/lsp/lsp_context_preprocessor_test.cpp
@@ -23,11 +23,14 @@
 
 using namespace hlasm_plugin::parser_library;
 using namespace hlasm_plugin::utils::resource;
+using namespace hlasm_plugin::parser_library::lsp;
 
-const resource_location MACRO_FILE("MAC");
-const resource_location SOURCE_FILE("OPEN");
-const resource_location COPY_FILE("MEMBER");
-const resource_location COPY_FILE2("MEMBER2");
+namespace {
+const resource_location mac_loc("MAC");
+const resource_location source_loc("OPEN");
+const resource_location member_loc("MEMBER");
+const resource_location member2_loc("MEMBER2");
+} // namespace
 
 class lsp_context_endevor_preprocessor_test : public testing::Test
 {
@@ -37,7 +40,7 @@ public:
             LR R2,R2)" },
             { "MEMBER2", R"(R5 EQU 5
             LR R5,R5)" } })
-        , a(contents, analyzer_options { SOURCE_FILE, &lib_provider, endevor_preprocessor_options() }) {};
+        , a(contents, analyzer_options { source_loc, &lib_provider, endevor_preprocessor_options() }) {};
 
     void SetUp() override { a.analyze(); }
     void TearDown() override {}
@@ -57,55 +60,54 @@ protected:
 TEST_F(lsp_context_endevor_preprocessor_test, go_to)
 {
     // no jump, instr -INC
-    EXPECT_EQ(location(position(1, 1), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(1, 1)));
+    EXPECT_EQ(location(position(1, 1), source_loc), a.context().lsp_ctx->definition(source_loc, position(1, 1)));
     // no jump, instr ++INCLUDE
-    EXPECT_EQ(location(position(2, 5), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(2, 5)));
+    EXPECT_EQ(location(position(2, 5), source_loc), a.context().lsp_ctx->definition(source_loc, position(2, 5)));
     // no jump, instr -INC
-    EXPECT_EQ(location(position(3, 1), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(3, 1)));
+    EXPECT_EQ(location(position(3, 1), source_loc), a.context().lsp_ctx->definition(source_loc, position(3, 1)));
 
     // jump from source to included file
-    EXPECT_EQ(location(position(0, 3), COPY_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(1, 8)));
+    EXPECT_EQ(location(position(0, 3), member_loc), a.context().lsp_ctx->definition(source_loc, position(1, 8)));
     // jump from source to included file
-    EXPECT_EQ(location(position(0, 3), COPY_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(2, 14)));
+    EXPECT_EQ(location(position(0, 3), member_loc), a.context().lsp_ctx->definition(source_loc, position(2, 14)));
     // jump from source to included file
-    EXPECT_EQ(location(position(0, 3), COPY_FILE2), a.context().lsp_ctx->definition(SOURCE_FILE, position(3, 8)));
+    EXPECT_EQ(location(position(0, 3), member2_loc), a.context().lsp_ctx->definition(source_loc, position(3, 8)));
 
     // no jump
-    EXPECT_EQ(location(position(1, 15), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(1, 15)));
+    EXPECT_EQ(location(position(1, 15), source_loc), a.context().lsp_ctx->definition(source_loc, position(1, 15)));
     // no jump
-    EXPECT_EQ(location(position(2, 21), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(2, 21)));
+    EXPECT_EQ(location(position(2, 21), source_loc), a.context().lsp_ctx->definition(source_loc, position(2, 21)));
     // no jump
-    EXPECT_EQ(location(position(3, 15), SOURCE_FILE), a.context().lsp_ctx->definition(SOURCE_FILE, position(3, 15)));
+    EXPECT_EQ(location(position(3, 15), source_loc), a.context().lsp_ctx->definition(source_loc, position(3, 15)));
 }
 
 TEST_F(lsp_context_endevor_preprocessor_test, refs)
 {
-    location_list expected_inc_locations { location(position(1, 0), SOURCE_FILE),
-        location(position(3, 0), SOURCE_FILE) };
-    location_list expected_include_locations { location(position(2, 0), SOURCE_FILE) };
-    location_list expected_member_locations { location(position(1, 6), SOURCE_FILE),
-        location(position(2, 11), SOURCE_FILE) };
-    location_list expected_member2_locations { location(position(3, 6), SOURCE_FILE) };
+    location_list expected_inc_locations { location(position(1, 0), source_loc), location(position(3, 0), source_loc) };
+    location_list expected_include_locations { location(position(2, 0), source_loc) };
+    location_list expected_member_locations { location(position(1, 6), source_loc),
+        location(position(2, 11), source_loc) };
+    location_list expected_member2_locations { location(position(3, 6), source_loc) };
     location_list expected_blabla_locations {};
 
     // -INC reference
-    EXPECT_EQ(expected_inc_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(1, 1)));
+    EXPECT_EQ(expected_inc_locations, a.context().lsp_ctx->references(source_loc, position(1, 1)));
     // ++INCLUDE reference
-    EXPECT_EQ(expected_include_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(2, 5)));
+    EXPECT_EQ(expected_include_locations, a.context().lsp_ctx->references(source_loc, position(2, 5)));
     // -INC reference
-    EXPECT_EQ(expected_inc_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(3, 2)));
+    EXPECT_EQ(expected_inc_locations, a.context().lsp_ctx->references(source_loc, position(3, 2)));
 
     // MEMBER reference
-    EXPECT_EQ(expected_member_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(1, 8)));
+    EXPECT_EQ(expected_member_locations, a.context().lsp_ctx->references(source_loc, position(1, 8)));
     // MEMBER reference
-    EXPECT_EQ(expected_member_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(2, 14)));
+    EXPECT_EQ(expected_member_locations, a.context().lsp_ctx->references(source_loc, position(2, 14)));
     // MEMBER reference
-    EXPECT_EQ(expected_member2_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(3, 8)));
+    EXPECT_EQ(expected_member2_locations, a.context().lsp_ctx->references(source_loc, position(3, 8)));
 
     // blabla reference
-    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(1, 15)));
+    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(source_loc, position(1, 15)));
     // blabla reference
-    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(2, 21)));
+    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(source_loc, position(2, 21)));
     // blabla reference
-    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(SOURCE_FILE, position(3, 15)));
+    EXPECT_EQ(expected_blabla_locations, a.context().lsp_ctx->references(source_loc, position(3, 15)));
 }
