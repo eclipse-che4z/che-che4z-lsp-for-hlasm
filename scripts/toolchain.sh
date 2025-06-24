309298f2d5bff83953cc94faaa5e01470eaf16ce
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2025-06-24 14:51:30 +0200

    ci: Bump tools and images

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -5,16 +5,13 @@ set -e
 apk update
 apk add git curl g++ bison make patch tar xz gawk
 
-git clone -n --filter=tree:0 https://gitlab.alpinelinux.org/alpine/aports.git /aports
-(cd /aports; git sparse-checkout set --no-cone main/gcc ; git checkout 1a03a2a9c9e77f1a07d48b6f6805e72c8c63c03f)
-
 mkdir /toolchain
 
 git clone https://github.com/richfelker/musl-cross-make.git
 
 cd musl-cross-make
 
-git checkout -f e149c31c48b4f4a4c9349ddf7bc0027b90245afc
+git checkout -f 6f3701d08137496d5aac479e3a3977b5ae993c1f
 
 echo "TARGET = $1" >> config.mak
 echo "OUTPUT = /toolchain/" >> config.mak
@@ -24,12 +21,7 @@ echo "DL_CMD = curl --retry 20 --retry-max-time 120 -C - -L -o" >> config.mak
 echo "COMMON_CONFIG += CFLAGS=\"-fdata-sections -ffunction-sections -O2 -g0\" CXXFLAGS=\"-fdata-sections -ffunction-sections -O2 -g0\"" >> config.mak
 echo "BINUTILS_CONFIG = --enable-gprofng=no" >> config.mak
 
-echo "d91ecc3d20ce6298bd95f9b09cc51dc6d3c73ae3 *gcc-14.2.0.tar.xz" > hashes/gcc-14.2.0.tar.xz.sha1
 echo "0e008260a958bbd10182ee3384672ae0a310eece *binutils-2.41.tar.xz" > hashes/binutils-2.41.tar.xz.sha1
-
-mkdir patches/gcc-14.2.0
-cp /aports/main/gcc/*.patch patches/gcc-14.2.0/
-cp ../scripts/gcc-0017.patch patches/gcc-14.2.0/0017-Alpine-musl-package-provides-libssp_nonshared.a.-We-.patch
 
 make -j 8
 make install
