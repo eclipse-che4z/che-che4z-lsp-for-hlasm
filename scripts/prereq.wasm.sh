1962dbc4d18bf62601c0b22e7ded4cad1e636f75
slavek-kucera <53339291+slavek-kucera@users.noreply.github.com>
2025-09-03 16:17:01 +0200

    ci: Run WebAssembly tests on big endian platform

--- scripts/prereq.wasm.sh
+++ scripts/prereq.wasm.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
-sudo apt-get update && sudo apt-get install -y ninja-build
+sudo apt-get update && sudo apt-get install -y ninja-build qemu-user libc6-s390x-cross libstdc++6-s390x-cross
 # install cmake v3.29.2
 (cd /tmp && curl -sSLo cmake-install.sh https://github.com/Kitware/CMake/releases/download/v3.29.2/cmake-3.29.2-linux-x86_64.sh && chmod +x cmake-install.sh && ./cmake-install.sh --skip-license --exclude-subdir --prefix=/usr/local)
 echo "PATH=/usr/local/bin:$PATH" >> $GITHUB_ENV
 cat scripts/4.0.10.patch | (cd /emsdk/upstream/emscripten; patch -p1)
+(NODE_VER=$(node --version) && mkdir /tmp/node-be && cd /tmp/node-be && curl -sSL https://nodejs.org/dist/$NODE_VER/node-$NODE_VER-linux-s390x.tar.xz | tar xJ --strip-components=1)
--- scripts/test-runner.wasm.sh
+++ scripts/test-runner.wasm.sh
@@ -2,3 +2,5 @@
 set -e
 node $1.js
 node --eval "require('./$1.js')({web:true,worker:{postMessage:()=>{}}})"
+qemu-s390x -L /usr/s390x-linux-gnu/ /tmp/node-be/bin/node $1.js
+qemu-s390x -L /usr/s390x-linux-gnu/ /tmp/node-be/bin/node --eval "require('./$1.js')({web:true,worker:{postMessage:()=>{}}})"
